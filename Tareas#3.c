d3077bfe1eb989778b32e84c51482d33e40a0943
JuanGonzalez00 <159209881+JuanGonzalez00@users.noreply.github.com>
2024-02-08 22:15:31 -0600

    Add files via upload

---
+++ Tareas#3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int main (){
+    bool x = true ;
+    bool y = false;
+    system ("cls");
+    printf("%d  \n",  x);
+    printf ("%d  \n", y);
+    return 0;
+}
